7dc87a2cb711858d27f4562b711fd587d09b8a50
jpleonardxyz <169338128+jpleonardxyz@users.noreply.github.com>
2024-10-16 08:34:07 +0200

    Add function for handling operation

--- javascript.js
+++ javascript.js
@@ -1,3 +1,25 @@
+//store variables for the parts of the calculation
+let operand1 = 0;
+let operand2 = 0;
+let operator = '';
+
+function operate(num1, num2, op){
+    //call appropriate operation function based on input operation
+    switch (op) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            break;
+    }
+}
+
+
 function add(num1, num2){
     return num1+num2;
 }
